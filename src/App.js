12e6e6f77aad72f1951bf24f29ea9ce41f98a6f0
maberizk <michelle.aberizk@gmail.com>
2023-10-18 17:50:46 -0700

    done for today

--- src/App.js
+++ src/App.js
@@ -11,84 +11,85 @@ import chessPicture from "./images/image-chess.webp";
 function App() {
   return (
     <div className="App">
-        <div className="headerContainer">
-          <div className="headerLeft">
-            <h1>Notifications</h1>
-            <span className="numOfNotifications">3</span>
-          </div>
+      <div className="headerContainer">
+        <div className="headerLeft">
+          <h1>Notifications</h1>
+          <span className="numOfNotifications">3</span>
+        </div>
         <div>
-
           <p className="markAsRead">Mark all as read</p>
         </div>
-        </div>
-        <div class="notifications">
-          <div class="notification">
-            <img src={markImg} alt="profileImg" />
-            <div className="noticationText">
-              <p>
-                <span className="name">Mark Webber</span> reacted to your recent
-                post <span>My first tournament today!</span>
-              </p>
-              <p>1m ago</p>
-            </div>
+      </div>
+      <div class="notifications">
+        <div class="notification">
+          <img src={markImg} alt="profileImg" />
+          <div className="noticationText">
+            <p>
+              <span className="name">Mark Webber</span> reacted to your recent
+              post <span className="postTitle">My first tournament today!</span>
+              <div className="redCircle"></div>
+            </p>
+            <p>1m ago</p>
           </div>
-          <div class="notification">
-            <img src={angelaImg} alt="profileImg" />
+        </div>
+        <div class="notification">
+          <img src={angelaImg} alt="profileImg" />
+          <div className="noticationText">
             <p>
               <span className="name">Angela Gray</span> followed you
             </p>
             <p>5m ago</p>
           </div>
-          <div class="notification">
-            <img src={jacobImg} alt="profileImg" />
+        </div>
+        <div class="notification">
+          <img src={jacobImg} alt="profileImg" />
+          <p>
+            <span className="name">Jacob Thompson</span> has joined your group
+            <span>Chess Club</span>
+          </p>
+          <p>1 day ago</p>
+        </div>
+        <div class="notification">
+          <img src={rizkyImg} alt="profileImg" />
+          <p>
+            <span className="name">Rizky Hasanuddin</span>sent you a private
+            message
+          </p>
+          <p>5 days ago</p>
+          <div className="message">
             <p>
-              <span className="name">Jacob Thompson</span> has joined your group
-              <span>Chess Club</span>
+              Hello, thanks for setting up the Chess Club. I've been a member
+              for a few weeks now and I'm already having lots of fun and
+              improving my game.
             </p>
-            <p>1 day ago</p>
-          </div>
-          <div class="notification">
-            <img src={rizkyImg} alt="profileImg" />
-            <p>
-              <span className="name">Rizky Hasanuddin</span>sent you a private
-              message
-            </p>
-            <p>5 days ago</p>
-            <div className="message">
-              <p>
-                Hello, thanks for setting up the Chess Club. I've been a member
-                for a few weeks now and I'm already having lots of fun and
-                improving my game.
-              </p>
-            </div>
-          </div>
-          <div class="notification">
-            <img src={kimImg} alt="profileImg" />
-            <p>
-              <span className="name">Kimberly Smith</span>commented on your
-              picture
-            </p>
-            <p>1 week ago</p>
-            <img src={chessPicture} alt="chess" />
-          </div>
-          <div class="notification">
-            <img src={nathanImg} alt="profileImg" />
-            <p>
-              <span className="name">Nathan Peterson</span> reacted to your
-              recent post{" "}
-              <span>5 end-game strategies to increase your win rate</span>
-            </p>
-            <p>2 weeks ago</p>
-          </div>
-          <div class="notification">
-            <img src={annaImg} alt="profileImg" />
-            <p>
-              <span className="name">Anna Kim</span> left the group{" "}
-              <span>Chess Club</span>
-            </p>
-            <p>2 weeks ago</p>
           </div>
         </div>
+        <div class="notification">
+          <img src={kimImg} alt="profileImg" />
+          <p>
+            <span className="name">Kimberly Smith</span>commented on your
+            picture
+          </p>
+          <p>1 week ago</p>
+          <img src={chessPicture} alt="chess" />
+        </div>
+        <div class="notification">
+          <img src={nathanImg} alt="profileImg" />
+          <p>
+            <span className="name">Nathan Peterson</span> reacted to your recent
+            post <span>5 end-game strategies to increase your win rate</span>
+          </p>
+          <p>2 weeks ago</p>
+        </div>
+        <div class="notification">
+          <img src={annaImg} alt="profileImg" />
+          <p>
+            <span className="name">Anna Kim</span> left the group{" "}
+            <span>Chess Club</span>
+          </p>
+          <p>2 weeks ago</p>
+        </div>
+      </div>
     </div>
   );
 }
